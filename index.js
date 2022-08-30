670a082c1f0b7f2236633b6c832a043e491f8dae
Emily Fernschild <emilyfernschild@gmail.com>
2022-08-30 15:43:04 -0400

    commit tree da7e253e969d336b93aa0097d2a021971c0e4036

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+
+ function saturdayFun(activitys = "roller-skate"){
+    return(`This Saturday, I want to ${activitys}!`);
+ }
+
+ const mondayWork = function (location = "go to the office"){
+    return (`This Monday, I will ${location}.`)
+ }
+
+ function wrapAdjective(highlight = "*"){
+    //const encouraging = wrapAdjective(highlight)(adjective)
+    return function (adjective = 'special') {
+        return (`You are ${highlight}${adjective}${highlight}!`)
+    }
+ }
